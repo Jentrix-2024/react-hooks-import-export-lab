e0801c3b32b9c6bf2a320feeecf38bbecd89463b
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-10-24 14:15:07 +0300

    Completed Assinment.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import NavBar from "src/components/NavBar.js";
+import Home from "src/components/Home.js";
+import About from "src/components/About.js";
+
 
 function App() {
   return (
